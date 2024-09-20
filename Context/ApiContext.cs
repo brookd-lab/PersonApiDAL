c1f7e4bf6ad188618b9487a63fd27bb8f7e94c71
David Brook <brookd2022@gmail.com>
2024-09-19 18:47:40 -0700

    Add project files.

---
+++ Context/ApiContext.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using PersonApiDAL.Models;
+
+namespace PersonApiDAL.Context
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+    } 
+}
---
+++ Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonApiDAL.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set;  }
+        public int Age { get; set; }
+    }   
+}
